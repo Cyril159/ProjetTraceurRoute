0d3e858ce7e67175b77f8228578b14317d9d03e7
Brisson Cyril <brissoncyril@yahoo.fr>
2021-01-12 21:04:23 +0100

    Add files via upload

---
+++ ProceduralGeneration/Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof(MapGenerator))]
+public class MapGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MapGenerator mapGen = (MapGenerator) target;
+
+        if (DrawDefaultInspector() && mapGen.autoUpdate)
+        {
+            mapGen.GenerateMap();
+        }
+
+        if (GUILayout.Button("Generate"))
+        {
+            mapGen.GenerateMap();
+        }
+
+        if (GUILayout.Button("Save"))
+        {
+            float [,] heightMap = mapGen.BuildMap();
+
+            MapRecorder.SaveMapAsTXT(heightMap, mapGen.savedFileName, mapGen.waterLevel);
+        }
+    }
+}
